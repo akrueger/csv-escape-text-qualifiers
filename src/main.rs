475c03406aba9e63e85cfc9f46e324e6bf07a345
Andrew Krueger <akrueger@adjacent-tech.com>
2020-11-01 22:03:55 -0600

    Use bstr for byte manipulation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-mod bytes;
-
-// use std::env;
-use crate::bytes::process_bytes;
+use std::{env,fs};
+use bstr::{ByteSlice,ByteVec};
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    //
-    // let input_file = &args[1];
-    // let output_file = &args[2];
-    //
-    // process_csv(input_file, output_file)
+    let args: Vec<String> = env::args().collect();
 
-    process_bytes();
+    let input_file = &args[1];
+    let output_file = &args[2];
+
+    let mut file_contents = fs::read(input_file).expect("Unable to read file");
+
+    file_contents.replace_range(..1, "<<< HEAD >>>");
+    let t1 = file_contents.replace("\r\n\"", "\r\n<<< START >>>")
+        .replace("\",\"", "<<< INNER >>>")
+        .replace("\"\r\n", "<<< END >>>")
+        .replace("\"", "\"\"")
+        .replace("<<< END >>>", "\"\r\n")
+        .replace("<<< INNER >>>", "\",\"")
+        .replace("<<< START >>>", "\"")
+        .replace("<<< HEAD >>>", "\"");
+
+    fs::write(output_file, t1).expect("Unable to write file");
 }
